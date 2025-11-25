ad1e28abd785c088e18dc906a20d3cc910471e5f
よし <jsoriaf2400@alumno.ipn.mx>
2025-11-25 10:21:58 -0600

    Hola Joshi

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -3,5 +3,6 @@ package org.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, CodeDynamite!");
+        System.out.println("Hello, Joshi-Armagedon");
     }
 }
