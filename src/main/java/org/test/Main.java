008ff46b03ed1df0bcdde67da0715fb862f7af71
Pancito00 <ashleymedinamontesdeoca@gmail.com>
2025-11-26 07:43:54 -0600

    Change greeting from 'Hello' to 'Adios' for Kiray
    Despedida a Kiray

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, CodeDynamite!");
         System.out.println("Hello, Joshi-Armagedon");
-        System.out.println("Hello, Kiray");
+        System.out.println("Adios, Kiray");
     }
 }
